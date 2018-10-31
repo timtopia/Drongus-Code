c7ef4ba2f236b6db5cf9fe6bb10dbe0275c7dc47
Timothy Zhang <timothyzhang6@gmail.com>
2018-10-31 12:13:19 -0400

    Vision

--- Vision/vision.py
+++ Vision/vision.py
@@ -1,14 +1,19 @@
 import numpy as np
 import cv2
+import argparse
 
 cap = cv2.VideoCapture(0)
 
 while(True):
 	ret, frame = cap.read()
 	
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	lower = np.array([17, 15, 100])
+	upper = np.array([75,75, 255])
+	mask = cv2.inRange(frame, lower, upper)
+	output = cv2.bitwise_and(frame, frame, mask = mask)
+
 	
-	cv2.imshow('frame',gray)
+	cv2.imshow('frame',output)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 		
