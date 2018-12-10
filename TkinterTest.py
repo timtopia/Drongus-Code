9803981f52a2cff808cd00020911ca1e71e5ba12
Julian Lioanag <jthemage101@gmail.com>
2018-12-10 13:11:00 -0500

    need to still fix

--- TkinterTest.py
+++ TkinterTest.py
@@ -1,8 +1,46 @@
 from Tkinter import *
+import MotorController
+import MyJoystick
+import time
 
 root = Tk()
-text = Text(root)
-text.insert(INSERT, "remo drive")
-text.pack()
+root.title("Drongus Debug")
+root.geometry('500x500')
+mc = MotorController.MotorController()
+joy = MyJoystick.Joystick()
+
+TL = DoubleVar()
+BL = DoubleVar()
+TR = DoubleVar()
+BR = DoubleVar()
+
+TOP_LEFT = Label(root, textvariable=TL)
+BOT_LEFT = Label(root, textvariable=BL)
+TOP_RIGHT = Label(root, textvariable=TR)
+BOT_RIGHT = Label(root, textvariable=BR)
+
+TOP_LEFT.grid(column=0, row=0)
+BOT_LEFT.grid(column=0, row=1)
+TOP_RIGHT.grid(column=1, row=0)
+BOT_RIGHT.grid(column=1, row=1)
+
+TL.set(0.5)
+
+while True:
+    joy = MyJoystick.Joystick()
+    values = joy.update()
+
+    TL.set(values["LEFT_X"])
+    BL.set(values["LEFT_Y"])
+    TR.set(values["RIGHT_X"])
+    BR.set(values["RIGHT_Y"])
+
+    mc.reset()
+    mc.roll(values["LEFT_X"])
+    mc.pitch(values["LEFT_Y"])
+    mc.yaw(values["RIGHT_X"])
+    mc.thrust(values["RIGHT_Y"])
+
+    time.sleep(0.1)
 
 root.mainloop()
