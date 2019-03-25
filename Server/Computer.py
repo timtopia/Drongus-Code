804ea37bec6e663a76b76df1850e0ca4e13300e9
Timtopia <timothyzhang6@gmail.com>
2019-03-25 11:39:20 -0400

    Server

---
+++ Server/Computer.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+HOST, PORT = "10.0.41.243", 9999
+data = "this here data wont send!! "
+
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+finally:
+    sock.close()
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
