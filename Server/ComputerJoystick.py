8f8bae4c84628f7e394d0d0ec93f217030033e21
Timtopia <timothyzhang6@gmail.com>
2019-03-28 12:28:04 -0400

    Stuff

--- Server/ComputerJoystick.py
+++ Server/ComputerJoystick.py
@@ -59,7 +59,7 @@ while done==False:
     try:
         # Connect to server and send data
         sock.connect((HOST, PORT))
-        sock.sendall(bytes(data + "\n", "utf-8"))
+        sock.sendall(Rotations)
 
         # Receive data from the server and shut down
         received = str(sock.recv(1024), "utf-8")
