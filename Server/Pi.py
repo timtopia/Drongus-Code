01cad7eae8adb86b86c221b26dba39774a0690fb
Timtopia <timothyzhang6@gmail.com>
2019-03-29 11:37:25 -0400

    Update Pi.py
    Test

--- Server/Pi.py
+++ Server/Pi.py
@@ -11,10 +11,11 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        '''print("{} wrote:".format(self.client_address[0]))
+        print(self.data)'''
         # just send back the same data, but upper-cased
         self.request.sendall(bytes("Recieved", "utf8"))
+        return self.data
 
 
 while True:
@@ -27,3 +28,5 @@ while True:
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+    print(server.handle())
+    
