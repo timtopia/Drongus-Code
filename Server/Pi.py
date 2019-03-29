a38302f80674ce36be451f53b68f30ebfdcfa9e6
Timtopia <timothyzhang6@gmail.com>
2019-03-29 12:08:13 -0400

    Update Pi.py
    Added Motor Stuff

--- Server/Pi.py
+++ Server/Pi.py
@@ -1,6 +1,38 @@
 import socketserver
+from gpiozero import Motor, OutputDevice
+
+default = 0
+
+class MotorConoller():
+    #Motor1 = Back Left
+    motor1 = Motor(24, 27)
+    motor1_enable = OutputDevice(5, initial_value=0)
+    #Motor2 = Back Right
+    motor2 = Motor(6, 22)
+    motor2_enable = OutputDevice(17, initial_value=0)
+    #Motor3 = Front Right
+    motor3 = Motor(23, 16)
+    motor3_enable = OutputDevice(12, initial_value=0)
+    #Motor4 = Front Left
+    motor4 = Motor(13, 18)
+    motor4_enable = OutputDevice(25, initial_value=0)
+
+    def Yaw(value):
+        
+    def Pitch(value):
+
+    def Roll(value):
+
+    def Thrust(value):
+        default += value:
+
+    def setMotors(self):
+        
+    
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
+    def __init__(self):
+        self.mc = MotorController()
     """
     The RequestHandler class for our server.
 
@@ -15,7 +47,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print(self.data)
         # just send back the same data, but upper-cased
         self.request.sendall(bytes("Recieved", "utf8"))
-
 while True:
     if __name__ == "__main__":
         HOST, PORT = "0.0.0.0", 9999
