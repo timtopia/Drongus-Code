2c69f9daf49b6bc8b80b4be40fd6d139c47dce20
Timtopia <timothyzhang6@gmail.com>
2019-04-03 12:39:36 -0400

    Update Pi.py

--- Server/Pi.py
+++ Server/Pi.py
@@ -86,6 +86,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         It is instantiated once per connection to the server, and must
         override the handle() method to implement communication to the
         client."""
+        print('Hello')
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
